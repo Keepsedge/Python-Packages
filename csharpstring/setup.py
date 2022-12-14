cb334128646796b0ff009c1951460aaaa8da36a8
J.Sullivan <energy.keeper@gmail.com>
2022-12-14 14:43:32 -0500

    version up.
    Also linked to Github source, and issue tracker

--- csharpstring/setup.py
+++ csharpstring/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.4"
 DESCRIPTION = "A String Package that emulates C# String manipulations."
 LONG_DESCRIPTION = "A Package that emulates the C# String Class With all instance and static members."
 
